1ff0bb0660c928f67f93feafb9ff1edd97d39316
Nicolas Landaeta <nico.landaeta@icloud.com>
2024-06-25 18:49:04 -0400

    sendmoney.js fixed

--- assets/js/transactions.js
+++ assets/js/transactions.js
@@ -1,19 +1,4 @@
 $(document).ready(function () {
-  $("#depositForm").on("submit", function (event) {
-    event.preventDefault(); // Previene la recarga
 
-    const monto = parseFloat($("#monto").val()); // Obtiene el input y se convierte a números
-    let balanceActual = parseFloat(localStorage.getItem("balance") || "0");
-
-    if (monto > 0) {
-      balanceActual += monto;
-      localStorage.setItem("balance", balanceActual.toFixed(2)); // Actualiza balance
-
-      alert("Depósito realizado con éxito.");
-      // Redirige a menu con el balance actualizado
-      window.location.href = "menu.html";
-    } else {
-      alert("Por favor, ingrese un monto válido.");
     }
-  });
-});
+  );;
